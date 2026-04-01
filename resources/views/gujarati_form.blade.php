1b43246217c7cfc7d354ba6b20718eb90d209a67
Shah-Kalp-5871 <kalp.5871@gmail.com>
2026-04-01 14:25:54 +0530

    yaya

--- resources/views/gujarati_form.blade.php
+++ resources/views/gujarati_form.blade.php
@@ -119,9 +119,10 @@
                                         <tr>
                                             <th>#</th>
                                             @php 
-                                                $cols = explode(',', $q->meta_params['columns'] ?? 'નામ,સંબંધ,ઉંમર,શિક્ષણ');
+                                                $colsRaw = $q->meta_params['columns'] ?? 'નામ,સંબંધ,ઉંમર,શિક્ષણ';
+                                                $cols = is_array($colsRaw) ? $colsRaw : explode(',', $colsRaw);
                                                 $rowsRaw = $q->meta_params['rows'] ?? '5';
-                                                $rows = is_numeric($rowsRaw) ? range(1, (int)$rowsRaw) : explode(',', $rowsRaw);
+                                                $rows = is_numeric($rowsRaw) ? range(1, (int)$rowsRaw) : (is_array($rowsRaw) ? $rowsRaw : explode(',', $rowsRaw));
                                             @endphp
                                             @foreach($cols as $col)
                                                 <th class="t" data-en="{{ trim($col) }}">{{ trim($col) }}</th>
